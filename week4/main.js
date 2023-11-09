45ba3fd2e4cf61c6b82e1a326d1d3b246397d9b7
vp2034 <vp2034@hw.ac.uk>
2023-11-09 16:55:08 +0400

    commit 2

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,17 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://vp2034.github.io/week4/cities1.json');
-ourRequest.onload = function() {
-    console.log(ourRequest.responseText); 
-};
-ourRequest.send();
+var btn = document.getElementById("btn"); 
+btn.addEventListener("click", function(){
+
+
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://vp2034.github.io/week4/cities1.json');
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData[0]); 
+    };
+    ourRequest.send();
+})
+
+function renderHTML(data){
+    var htmlString = "this is a test"; 
+    cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+}
